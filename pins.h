21c96105abf61074f2b00c83826b1a4b6e7ded91
WiseLord <wiselord1983@gmail.com>
2019-03-24 10:24:53 +0300

    Remove unused definitions

--- pins.h
+++ pins.h
@@ -49,19 +49,6 @@ void pinsInit(void);
 #define LED3_Port               GPIOA
 #define LED3_Pin                LL_GPIO_PIN_3
 
-#define BTN_0_Pin               LL_GPIO_PIN_8
-#define BTN_1_Pin               LL_GPIO_PIN_9
-#define BTN_2_Pin               LL_GPIO_PIN_10
-#define BTN_3_Pin               LL_GPIO_PIN_11
-#define BTN_4_Pin               LL_GPIO_PIN_12
-#define BTN_5_Pin               LL_GPIO_PIN_13
-#define ENC_A_Pin               LL_GPIO_PIN_14
-#define ENC_B_Pin               LL_GPIO_PIN_15
-
-#define INPUT_Port              GPIOB
-#define INPUT_Pin               (BTN_0_Pin | BTN_1_Pin | BTN_2_Pin | BTN_3_Pin | \
-                                 BTN_4_Pin | BTN_5_Pin | ENC_A_Pin | ENC_B_Pin)
-
 // TFT LCD pins
 #define DISP_DATA_HI_Port       GPIOB
 #define DISP_DATA_HI_Pin        (LL_GPIO_PIN_8 | LL_GPIO_PIN_9 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | \
@@ -69,23 +56,6 @@ void pinsInit(void);
 #define DISP_DATA_LO_Port       GPIOC
 #define DISP_DATA_LO_Pin        (LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3 | \
                                  LL_GPIO_PIN_4 | LL_GPIO_PIN_5 | LL_GPIO_PIN_6 | LL_GPIO_PIN_7)
-
-#define DISP_D8_Port            GPIOB
-#define DISP_D8_Pin             LL_GPIO_PIN_8
-#define DISP_D9_Port            GPIOB
-#define DISP_D9_Pin             LL_GPIO_PIN_9
-#define DISP_D10_Port           GPIOB
-#define DISP_D10_Pin            LL_GPIO_PIN_10
-#define DISP_D11_Port           GPIOB
-#define DISP_D11_Pin            LL_GPIO_PIN_11
-#define DISP_D12_Port           GPIOB
-#define DISP_D12_Pin            LL_GPIO_PIN_12
-#define DISP_D13_Port           GPIOB
-#define DISP_D13_Pin            LL_GPIO_PIN_13
-#define DISP_D14_Port           GPIOB
-#define DISP_D14_Pin            LL_GPIO_PIN_14
-#define DISP_D15_Port           GPIOB
-#define DISP_D15_Pin            LL_GPIO_PIN_15
 
 // TFT parallel (8-bit) wiring
 #define DISP_CS_Port            GPIOC
@@ -107,11 +77,6 @@ void pinsInit(void);
 #define DISP_SPI_DC             DISP_RS
 #define DISP_SPI_SDI            DISP_WR
 #define DISP_SPI_SDO            DISP_RD
-
-// TFT I2C (SSD1306) wiring
-#define DISP_I2C_Port           GPIOC
-#define DISP_I2C_SCK_Pin        DISP_RS_Pin
-#define DISP_I2C_SDA_Pin        DISP_RST_Pin
 
 // USART used for debugging
 #define USART_DBG               USART1
