5912c887c4d5d341c8414e2001338d14569a6ea4
WiseLord <wiselord1983@gmail.com>
2019-08-23 22:23:31 +0300

    Update utils

--- utils.c
+++ utils.c
@@ -2,6 +2,7 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
 
 static char strbuf[256];    // String buffer
 
@@ -15,4 +16,26 @@ char *utilMkStr(const char *fmt, ...)
     va_end (args);
 
     return buffer;
+}
+
+bool utilReadChar(LineParse *lp, char ch)
+{
+    switch (ch) {
+    case '\n':
+    case '\r':
+        if (lp->idx == 0) {
+            break;
+        }
+        lp->line[lp->idx] = 0;
+        lp->idx = 0;
+        return true;
+    default:
+        lp->line[lp->idx++] = ch;
+        if (lp->idx >= LINE_SIZE) {
+            lp->line[--lp->idx] = 0;
+        }
+        break;
+    }
+
+    return false;
 }
