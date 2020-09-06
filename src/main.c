cc4650111626ef5037fee536a5ceb105f8168f1d
WiseLord <wiselord1983@gmail.com>
2020-09-07 00:02:43 +0300

    Fix for using dipsdrv functions

--- src/main.c
+++ src/main.c
@@ -96,6 +96,11 @@ void SysTick_Handler(void)
 {
 }
 
+#ifdef _DISP_READ_ENABLED
+void dispdrvReset(void);
+void dispdrvReadReg(uint16_t reg, uint16_t *args, uint8_t nArgs);
+#endif
+
 void printDispRegs(void)
 {
 #ifdef _DISP_READ_ENABLED
