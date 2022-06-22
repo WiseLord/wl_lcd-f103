b151f855c35c21b05965f48bc7cd73e3f072abe7
WiseLord <wiselord1983@gmail.com>
2022-06-22 15:49:19 +0300

    Remove unused code

--- src/main.c
+++ src/main.c
@@ -101,111 +101,10 @@ void SysTick_Handler(void)
     vacUpdateTimers();
 }
 
-#ifdef _DISP_READ_ENABLED
-void dispdrvReset(void);
-void dispdrvReadReg(uint16_t reg, uint16_t *args, uint8_t nArgs);
-#endif
-
-void printDispRegs(void)
-{
-#ifdef _DISP_READ_ENABLED
-
-    const uint8_t num = 10;
-
-    uint16_t args[num];
-    char buf[8];
-
-    for (uint16_t reg = 0x00; reg <= 0xFF; reg++) {
-        dispdrvReset();
-        dispdrvReadReg(reg, args, num);
-
-        bool hasData = false;
-        for (uint8_t i = 0; i < num; i++) {
-            if (args[i]) {
-                hasData = true;
-                break;
-            }
-        }
-
-        snprintf(buf, sizeof(buf), "%02x: ", reg);
-        usartSendString(USART_DBG, buf);
-
-        if (!hasData) {
-            usartSendString(USART_DBG, "\r");
-            continue;
-        }
-
-        for (uint8_t i = 0; i < num; i++) {
-            if (args[i]) {
-                snprintf(buf, sizeof(buf), "%04x ", args[i]);
-                usartSendString(USART_DBG, buf);
-            } else {
-                usartSendString(USART_DBG, "---- ");
-            }
-        }
-        usartSendString(USART_DBG, "\n\r");
-    }
-#else
-    usartSendString(USART_DBG, "\rRead pin is disabled\r\n");
-#endif
-}
-
-typedef struct {
-    color_t color;
-    char *name;
-} ColorNames;
-
-static ColorNames cn[] = {
-    {COLOR_WHITE,   "White"},
-    {COLOR_RED,     "Red"},
-    {COLOR_YELLOW,  "Yellow"},
-    {COLOR_LIME,    "Lime"},
-    {COLOR_AQUA,    "Aqua"},
-    {COLOR_BLUE,    "Blue"},
-    {COLOR_MAGENTA, "Magenta"},
-};
-
-static uint8_t rxBuf[32];
-static uint8_t resBuf[32];
-
-void rx_cb(int16_t bytes)
-{
-    if (bytes > 32) {
-        bytes = 32;
-    }
-
-    memcpy(resBuf, rxBuf, bytes);
-
-    for (int16_t i = 0; i < bytes; i++) {
-        resBuf[i] = toupper(resBuf[i]);
-    }
-}
-
 int main(void)
 {
     sysInit();
-
     vacInit();
-
-#ifndef _DISP_16BIT
-#if DISP_DATA_Pin & 0x00FF
-    i2cInit(I2C_MASTER, 100000, 0);
-    i2cInit(I2C_SLAVE, 100000, 0x28);
-#endif
-#endif
-
-#ifndef _DISP_16BIT
-#if DISP_DATA_Pin & 0x00FF
-    ks0066Init();
-
-    ks0066SetXY(0, 0);
-    ks0066WriteString("Color:");
-#endif
-#endif
-
-    usartInit(USART_DBG, 115200);
-    usartSendString(USART_DBG, "\rUsart init done\r\n");
-//    printDispRegs();
 
     glcdInit(GLCD_LANDSCAPE);
     glcdSetBacklight(true);
@@ -214,136 +113,27 @@ int main(void)
     int16_t w = glcdGet()->rect.w;
     int16_t h = glcdGet()->rect.h;
 
-    glcdDrawRect(0, 0, w, h, COLOR_BLACK);
+    glcdDrawRect(0, 0, w, h, COLOR_WHITE);
+    glcdSetFontColor(COLOR_BLACK);
+    glcdSetFontBgColor(COLOR_WHITE);
 
-    if (h >= 240) {
-        glcdSetFont(&fontterminus32);
-    } else if (h >= 176) {
-        glcdSetFont(&fontterminus20);
-    } else {
-        glcdSetFont(&fontterminus12);
-    }
-    font7segLoad(font_7seg_3);
-
-    int16_t tw = w / 16;
-    int16_t th = h / 4;
-
-    glcdDrawRect(w / 2 + tw * 1, h / 8 * 5, tw / 4 * 6, th, COLOR_RED);
-    glcdDrawRect(w / 2 + tw * 3, h / 8 * 5, tw / 4 * 6, th, COLOR_LIME);
-    glcdDrawRect(w / 2 + tw * 5, h / 8 * 5, tw / 4 * 6, th, COLOR_BLUE);
-
-    int16_t rx = w / 4 * 3;
-    int16_t ry = h / 4 * 1;
-    int16_t rr = ry - 8;
-    glcdDrawRing(rx, ry, ry - 2, 3, COLOR_WHITE);
-    glcdSync();
-
-    static char txBuf[8];
-
-#ifndef _DISP_16BIT
-#if DISP_DATA_Pin & 0x00FF
-    i2cSetRxCb(I2C_SLAVE, rx_cb);
-    i2cBegin(I2C_SLAVE, 0x28);
-    i2cSlaveTransmitReceive(I2C_SLAVE, rxBuf, sizeof(rxBuf));
-#endif
-#endif
-
-    char buf[32];
-
-#ifdef _MN14032
-    timerInit(TIM_DISPSCAN, 149, 99); // 10kHz timer
-    while (1) {
-        glcdSetFontColor(COLOR_WHITE);
-        glcdSetXY(0, -2);
-        glcdWriteString("Hello, world, it's a long string");
-        glcdSetXY(0, 10);
-        glcdWriteString("Hello, world, it's a long string");
-        glcdSetXY(0, 22);
-        glcdWriteString("Hello, world, it's a long string");
-        LL_mDelay(1000);
-        glcdDrawRect(0, 0, 140, 32, COLOR_BLACK);
-        LL_mDelay(100);
-        glcdDrawLine(0, 0, 139, 31, COLOR_WHITE);
-        glcdDrawLine(0, 31, 139, 0, COLOR_WHITE);
-        LL_mDelay(1000);
-    }
-#endif
-
-    glcdSetFontColor(COLOR_WHITE);
-    glcdSetFontBgColor(COLOR_NERO);
+    glcdSetFont(&fontterminus32);
+    font7segLoad(font_7seg_10);
 
     vacSetTimer(1000 * 20);
     vacSetState(VAC_ON);
 
     while (1) {
-        glcdSetXY(0, 0);
+        glcdSetXY(20, 20);
         int32_t time = vacGetTimer() / 1000;
 
         int32_t min = time / 60;
         int32_t sec = time % 60;
 
+        char buf[32];
         snprintf(buf, sizeof(buf), "%02ld:%02ld", min, sec);
-        glcdWriteString(buf);
-    }
-
-    while (1) {
-        static size_t it = 0;
-        if (++it >= sizeof(cn) / sizeof (cn[0])) {
-            it = 0;
-        }
-
-        static uint16_t cnt = 0;
-        cnt++;
-
-        memcpy(txBuf, cn[it].name, 8);
-
-#ifndef _DISP_16BIT
-#if DISP_DATA_Pin & 0x00FF
-        i2cBegin(I2C_MASTER, 0x28);
-        for (size_t i = 0; i <= strlen(txBuf); i++) {
-            i2cSend(I2C_MASTER, txBuf[i]);
-        }
-        i2cTransmit(I2C_MASTER);
-#endif
-#endif
-
-        glcdDrawCircle(rx, ry, rr, cn[it].color);
-
-        glcdSetFontColor(COLOR_WHITE);
-        glcdSetFontBgColor(COLOR_NERO);
-
-        glcdSetXY(2, h / 16 * 1);
-        snprintf(buf, sizeof(buf), "%2d.%03d", cnt / 1000, cnt % 1000);
+//        glcdWriteString(buf);
         font7segWriteString(buf);
-
-        glcdSetFontColor(cn[it].color);
-        glcdSetFontBgColor(COLOR_BLACK);
-
-        glcdSetXY(2, h / 16 * 5);
-        snprintf(buf, sizeof(buf), "Iteration: %d ", it);
-        glcdWriteString(buf);
-
-        glcdSetXY(2, h / 16 * 9);
-        snprintf(buf, sizeof(buf), "Tx: %-8s", txBuf);
-        glcdWriteString(buf);
-
-        glcdSetXY(2, h / 16 * 13);
-        snprintf(buf, sizeof(buf), "Rx: %-8s", resBuf);
-        glcdWriteString(buf);
-
-#ifndef _DISP_16BIT
-#if DISP_DATA_Pin & 0x00FF
-        ks0066SetXY(0, 1);
-        snprintf(buf, sizeof(buf), "%-8s", cn[it].name);
-        ks0066WriteString(buf);
-#endif
-#endif
-
-        snprintf(buf, sizeof(buf), "Color: %s\r\n", cn[it].name);
-        usartSendString(USART_DBG, buf);
-        glcdSync();
-
-        LL_mDelay(500);
     }
 
     return 0;
